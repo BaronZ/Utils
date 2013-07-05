3cea56fe78e262f40117f34e4bbf48eccc536ff0
Baron <realbo89@outlook.com>
2013-07-05 10:13:29 +0800

    added: convert date to timemillis

--- DateDemos.java
+++ DateDemos.java
@@ -7,8 +7,13 @@ class DateDemos{
 	public static void main(String[] args) {
 		
 		/*2013-07-02*/
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		System.out.println(sdf.format(new Date()));
-		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+		System.out.println(sdf1.format(new Date()));
+
+		/*convert date to timemillis*/
+		String date1 = "2013/7/5";
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/M/d");
+		//output:1372953600000
+		System.out.println(sdf2.parse(date1).getTime());		
 	}
 }
