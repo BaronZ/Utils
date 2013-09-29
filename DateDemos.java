6f0aac5929e2a0d9ecdfaedc459d4c7789a5a3fb
Baron <realbo89@outlook.com>
2013-09-29 12:22:55 +0800

    get month and year by millis

--- DateDemos.java
+++ DateDemos.java
@@ -4,6 +4,27 @@
  */
 class DateDemos{
 	
+	
+	/**
+	 * Description:根据毫秒数获取月份 
+	 * @created 2013-9-29 下午12:03:30 
+	 * @author ZZB
+	 */
+	public static int getMonth(long millis){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.get(Calendar.MONTH) + 1;
+	}
+	/**
+	 * Description:根据毫秒数获取年份 
+	 * @created 2013-9-29 下午12:15:47 
+	 * @author ZZB
+	 */
+	public static int getYear(long millis){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(millis);
+		return calendar.get(Calendar.YEAR);
+	}
 	public static void main(String[] args) {
 		
 		/*2013-07-02*/
